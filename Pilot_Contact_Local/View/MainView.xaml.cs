19d548ffb886a511f7ca004066e9b66a35e5f4b1
shaunliz.dev <shaunliz.dev@gmail.com>
2015-12-09 17:10:45 +0900

    Excel export

--- Pilot_Contact_Local/View/MainView.xaml.cs
+++ Pilot_Contact_Local/View/MainView.xaml.cs
@@ -13,13 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Pilot_Contact_Local.Model;
 using Pilot_Contact_Local.Service;
+using Microsoft.Office.Interop.Excel;
 
 namespace Pilot_Contact_Local.View
 {
     /// <summary>
     /// MainView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainView : Window
+    public partial class MainView : System.Windows.Window
     {
         List<Person> personList;
 
@@ -74,5 +75,95 @@ namespace Pilot_Contact_Local.View
         {
             ;
         }
+
+        private void btnSaveToExcel_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+
+            saveFileDialog.CreatePrompt = true;
+            saveFileDialog.OverwritePrompt = true;
+
+            saveFileDialog.DefaultExt = "*.xls";
+            saveFileDialog.Filter = "Excel Files (*.xls)|*.xls";
+            //saveFileDialog.InitialDirectory = "C:\\";
+            // 내문서 위치로
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); 
+
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    object missingType = Type.Missing;
+                    Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                    Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Add(missingType);
+                    Microsoft.Office.Interop.Excel.Worksheet excelWorksheet = (Microsoft.Office.Interop.Excel.Worksheet)excelBook.Worksheets.Add(missingType, missingType, missingType, missingType);
+                    excelApp.Visible = false;
+
+                    // 내용 저장
+                    // excelWorksheet.Cells[1, 2] = "TEST";
+
+                    /* Title 정리 */
+                    excelWorksheet.Cells[1, 1] = "ID";
+                    excelWorksheet.Cells[1, 2] = "Name";
+                    excelWorksheet.Cells[1, 3] = "Email";
+                    excelWorksheet.Cells[1, 4] = "Mobile Phone";
+                    excelWorksheet.Cells[1, 5] = "Telephone";
+                    excelWorksheet.Cells[1, 6] = "Fax.";
+                    excelWorksheet.Cells[1, 7] = "Address";
+                    excelWorksheet.Cells[1, 8] = "Memo";
+
+                    for (int i = 0; i < personList.Count; i++ )
+                    {
+                        excelWorksheet.Cells[2 + i, 1] = personList[i].Id.ToString();
+                        excelWorksheet.Cells[2 + i, 2] = personList[i].Name;
+                        excelWorksheet.Cells[2 + i, 3] = personList[i].Email;
+                        excelWorksheet.Cells[2 + i, 4] = personList[i].MobilePhone;
+                        excelWorksheet.Cells[2 + i, 5] = personList[i].TelePhone;
+                        excelWorksheet.Cells[2 + i, 6] = personList[i].FaxNumber;
+                        excelWorksheet.Cells[2 + i, 7] = personList[i].Address;
+                        excelWorksheet.Cells[2 + i, 8] = personList[i].Memo;
+
+                    }
+
+                    excelBook.SaveAs(@saveFileDialog.FileName, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal,
+                        missingType, missingType, missingType, missingType, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                        missingType, missingType, missingType, missingType, missingType);
+
+                    excelApp.Visible = true;
+
+                    excelBook.Close(missingType, missingType, missingType);
+                    excelApp.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelWorksheet);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBook);
+
+                }
+                catch 
+                {
+                    ;
+                }
+            }
+        }
+
+        #region 메모리해제
+        private static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception e)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+        #endregion
+
+
     }
 }
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Simple contacts program written in WPF
 # Feature
 1. Two Way Binding Sample
 2. SQLite DB
+3. Excel export
 
 # Setting
 1. Copy 'pilotcontacts.s3db' file in "D:/SqliteDB/PilotContacts" folder or edit <connectionStrings> in 'App.config'
